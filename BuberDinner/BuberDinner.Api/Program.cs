fd39e70298782163ceabfc4948a867a43f53d109
Jeroen Savat <jeroen.savat@lansweeper.com>
2022-07-29 12:05:09 +0200

    feat: first commit

---
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -0,0 +1,14 @@
+using BuberDinner.Application.Services.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
