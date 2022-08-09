1aa718846d105224da3942f4cac3f7903975a400
Jeroen Savat <jeroen.savat@lansweeper.com>
2022-08-09 16:20:54 +0200

    errorhandling end video

--- BuberDinner/BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
+++ BuberDinner/BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,5 +1,6 @@
 namespace BuberDinner.Api.Filters;
 
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -9,10 +10,14 @@ public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
     {
         var exception = context.Exception;
 
-        context.Result = new ObjectResult(new { error = "An error occured while processing your request." })
+        var problemDetails = new ProblemDetails
         {
-            StatusCode = 500
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+            Title = "An error occured while processing your request.",
+            Status = (int)HttpStatusCode.InternalServerError,
         };
+
+        context.Result = new ObjectResult(problemDetails);
 
         context.ExceptionHandled = true;
     }
--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,18 +1,30 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration)
-        .AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+        .AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
     // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+    /*
+        app.Map("/error", (HttpContext httpContext) =>
+        {
+            Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            return Results.Problem();
+        });*/
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
